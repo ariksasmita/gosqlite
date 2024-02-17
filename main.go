ef7362c2c9fe1359eb458139f1e0f56fa4f2d2ee
Arik Sasmita <ariksasmita@gmail.com>
2024-02-17 13:57:54 +0800

    feat: query person & person by id

--- main.go
+++ main.go
@@ -1,36 +1,63 @@
-package  main
+package main
 
 import (
-  "net/http"
-  "github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"personweb/models"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-  r := gin.Default()
+	err := models.ConnectDatabase()
+	checkErr(err)
+	r := gin.Default()
 
-  // API v1
-  v1 := r.Group("/api/v1")
-  {
-    v1.GET("person", getPersons)
-    v1.GET("person/:id", getPersonById)
-    v1.POST("person", addPerson)
-    v1.PUT("person/:id", updatePerson)
-    v1.DELETE("person/:id", deletePerson)
-    v1.OPTIONS("person", options)
-  }
-  
-  // By defaul   it serves on 8080 unless a
-  // PORT environment variable was defined
-  r.Run()
+	// API v1
+	v1 := r.Group("/api/v1")
+	{
+		v1.GET("person", getPersons)
+		v1.GET("person/:id", getPersonById)
+		v1.POST("person", addPerson)
+		v1.PUT("person/:id", updatePerson)
+		v1.DELETE("person/:id", deletePerson)
+		v1.OPTIONS("person", options)
+	}
+
+	// By defaul   it serves on 8080 unless a
+	// PORT environment variable was defined
+	r.Run()
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func getPersons(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "getPersons Called"})
+	persons, err := models.GetPersons(10)
+	checkErr(err)
+
+	if persons == nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "No Records Found"})
+		return
+	} else {
+		c.JSON(http.StatusOK, gin.H{"data": persons})
+	}
 }
 
 func getPersonById(c *gin.Context) {
 	id := c.Param("id")
-	c.JSON(http.StatusOK, gin.H{"message": "getPersonById " + id +" Called"})
+	person, err := models.GetPersonById(id)
+	checkErr(err)
+	// if the name is blank we can assume nothing is found
+	if person.FirstName == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "No Records Found"})
+		return
+	} else {
+		c.JSON(http.StatusOK, gin.H{"data": person})
+	}
 }
 
 func addPerson(c *gin.Context) {
